bec8bcfb17b3e08f087207e2201f6db805d0260f
Maciej Koczot <mefiuea@gmail.com>
2022-04-26 16:58:06 +0200

    add static file storage to settings

--- config/settings.py
+++ config/settings.py
@@ -150,6 +150,8 @@ STATICFILES_FINDERS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

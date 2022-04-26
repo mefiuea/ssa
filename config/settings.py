16d822fe9ddcb963dc3c3517389a20f9aaa4a890
Maciej Koczot <mefiuea@gmail.com>
2022-04-26 17:04:13 +0200

    change STATICFILES_STORAGE in settings

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,8 @@ STATICFILES_FINDERS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

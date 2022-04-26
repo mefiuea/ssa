35d27f11a419508ed0eaceebcd22a0dfb3f47d63
Maciej Koczot <mefiuea@gmail.com>
2022-04-26 11:26:55 +0200

    add try except (< 3 events/posts on main page)

--- home_page_app/views.py
+++ home_page_app/views.py
@@ -11,36 +11,38 @@ def home_page_view(request):
         pass
 
     if request.method == 'GET':
+        try:
+            events_instance = Events.objects.all().order_by('-date')[:3]
 
-        events_instance = Events.objects.all().order_by('-date')[:3]
+            # get all posts from database ordered by data
+            posts_instance = Post.objects.all().order_by('-created_date')
 
-        # get all posts from database ordered by data
-        posts_instance = Post.objects.all().order_by('-created_date')
+            # empty list to store all profiles connected with current post
+            profiles_users_list = []
+            for pi in posts_instance:
+                profile_user_instance = Profile.objects.get(owner=pi.owner)
+                profiles_users_list.append(profile_user_instance)
 
-        # empty list to store all profiles connected with current post
-        profiles_users_list = []
-        for pi in posts_instance:
-            profile_user_instance = Profile.objects.get(owner=pi.owner)
-            profiles_users_list.append(profile_user_instance)
+            # generate random string for templates for unique collapse id (only letters)
+            unique_id_list = []
+            for _ in range(len(profiles_users_list)):
+                unique_id_list.append(get_random_string(8))
 
-        # generate random string for templates for unique collapse id (only letters)
-        unique_id_list = []
-        for _ in range(len(profiles_users_list)):
-            unique_id_list.append(get_random_string(8))
+            # setting Pagination
+            paginator_instance = Paginator(posts_instance, 3)
+            page = request.GET.get('page')
+            posts_instance_paginator = paginator_instance.get_page(page)
+            nums = 'i' * posts_instance_paginator.paginator.num_pages
 
-        # setting Pagination
-        paginator_instance = Paginator(posts_instance, 3)
-        page = request.GET.get('page')
-        posts_instance_paginator = paginator_instance.get_page(page)
-        nums = 'i' * posts_instance_paginator.paginator.num_pages
+            # combination of three instances: the posts(from paginator), corresponding profile (the creator of the post)
+            # and unique number for bootstrap template
+            posts_profiles_unique = zip(posts_instance_paginator, profiles_users_list, unique_id_list)
 
-        # combination of three instances: the posts(from paginator), corresponding profile (the creator of the post) and
-        # unique number for bootstrap template
-        posts_profiles_unique = zip(posts_instance_paginator, profiles_users_list, unique_id_list)
-
-        return render(request, 'home_page_app/home.html', context={'events_instance': events_instance,
-                                                                   'posts_instance': posts_instance,
-                                                                   'nums': nums,
-                                                                   'posts_and_profiles_and_unique': posts_profiles_unique,
-                                                                   'posts_instance_paginator': posts_instance_paginator,
-                                                                   })
+            return render(request, 'home_page_app/home.html', context={'events_instance': events_instance,
+                                                                       'posts_instance': posts_instance,
+                                                                       'nums': nums,
+                                                                       'posts_and_profiles_and_unique': posts_profiles_unique,
+                                                                       'posts_instance_paginator': posts_instance_paginator,
+                                                                       })
+        except:
+            return render(request, 'home_page_app/home_empty.html')

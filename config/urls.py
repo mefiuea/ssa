6f00287ca98bf7fcf4369c4337baec3535f04a44
Maciej Koczot <mefiuea@gmail.com>
2022-04-27 17:51:46 +0200

    upgrade env-default. Change debug to False

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ from config import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('RLuZbg37/', admin.site.urls),
     path('', include('home_page_app.urls')),
     path('users/', include('users_app.urls')),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users_app/reset_password.html'),
